cf2bfb2fce88ab9479e2e2fcf2cec356e274d07f
brunooborgess <brunoodiass26@gmail.com>
2025-02-18 12:42:18 +0000

    codigo final

--- xadrez.c
+++ xadrez.c
@@ -5,17 +5,55 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
+
+    int numero;
+
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    
+    printf("--- Movimentação de peças ---\n");
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
 
+    do {
+        printf("Digite o numero de casas para o Bispo andar:\n");
+        scanf("%d", &numero);
+
+        if (numero < 5 || numero > 5) {
+            printf("Numero de casas incorreto!\n");
+        }
+    } while (numero != 5); // Continua pedindo até que o usuário digite 5
+
+    printf("Bispo andou %d casas para diagonal superior direita\n", numero);
+
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
 
+    do {
+        printf("Digite o numero de casas para a Torre andar:\n");
+        scanf("%d", &numero);
+
+        if (numero < 5 || numero > 5) {
+            printf("Numero de casas incorreto!\n");
+        }
+    } while (numero != 5); // Continua pedindo até que o usuário digite 5
+
+    printf("Torre andou %d casas para a direita\n", numero);
+
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+
+    do {
+        printf("Digite o numero de casas para a Rainha andar:\n");
+        scanf("%d", &numero);
+
+        if (numero < 8 || numero > 8) {
+            printf("Numero de casas incorreto!\n");
+        }
+    } while (numero != 8); // Continua pedindo até que o usuário digite 8
+
+    printf("Rainha andou %d casas para a esquerda\n", numero);
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
